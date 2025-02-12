a2d8e77bc11beac52d7e74206e2f5ed0412fac22
jeevaninit <jeevaninit@gmail.com>
2025-02-12 17:00:22 +0530

    adding files

---
+++ printingMethods.js
@@ -0,0 +1,8 @@
+/* var,let,const - to define a variable  */
+
+var title='Welcome to Printing Methods';
+var classtime='7 Am';
+var course='Javascript and React';
+var fee='20000';
+var headinglevel2='Printing Method by InnerHTML';
+var btn='Sign In'
---
+++ script.js
@@ -0,0 +1,3 @@
+function bgcolorred(){
+    document.bgColor='red';
+}
